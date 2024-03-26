088bbb34c6619fc0f401e9cce656550308affc6c
ErickSuirenAki <ericksuirenaki@gmail.com>
2024-03-26 09:13:19 -0400

    problema

--- Gpm/src/components/DescricaoProblema/DescricaoProblema.jsx
+++ Gpm/src/components/DescricaoProblema/DescricaoProblema.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import './DescricaoProblema.css'
 const DescricaoProblema = () => {
   return (
-    <div>DescricaoProblema</div>
-  )
+    <div>DescricaoProblema
+      <h1>Problema</h1>
+      <p class="paragrafo"> O Grupo de Pesquisa GP Mecatrônica realiza projetos voltados à robótica educacional,<br />
+                           à manipulação de resíduos eletrônicos, à compostagem doméstica, à inserção de meninas<br />
+                           no campo científico, dentre muitos outros projetos, no entanto, no entanto, enfrenta um <br />
+                           desafio significativo de visibilidade. Existe uma lacuna considerável entre as realizações <br />
+                           e inovações ocorridas dentro do grupo e o conhecimento externo sobre essas iniciativas.<br /> A falta 
+                           de divulgação eficaz limita o impacto potencial dos projetos, impedindo a disseminação<br /> de conhecimento,
+                           colaborações e reconhecimento externo. A ausência de uma plataforma de <br /> divulgação eficiente resulta em um
+                           desconhecimento generalizado sobre os projetos e conquistas <br />  do GP Mecatrônica fora do âmbito do grupo, impede  
+                           a atração de colaboradores externos interessados <br /> nos temas abordados pelo grupo e alunos que tenham interesse em
+                           participar de projetos. A colaboração <br />interdisciplinar é crucial para o avanço da pesquisa, e a falta de visibilidade
+                           pode limitar as oportunidades. </p>
+      
+    </div>)
 }
 
 export default DescricaoProblema
